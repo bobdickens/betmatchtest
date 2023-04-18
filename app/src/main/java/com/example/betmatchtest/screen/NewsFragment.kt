50236870037106489d8bfcc41ca852bd10c93b82
Anton <imladris.doc@gmail.com>
2023-04-18 20:32:52 +0300

    three commit

--- app/src/main/java/com/example/betmatchtest/screen/NewsFragment.kt
+++ app/src/main/java/com/example/betmatchtest/screen/NewsFragment.kt
@@ -6,10 +6,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.betmatchtest.R
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.betmatchtest.data.News
 import com.example.betmatchtest.databinding.FragmentNewsBinding
+import com.example.betmatchtest.domain.ApiInterface
+import com.example.betmatchtest.domain.NewsAdapter
 
-import com.example.betmatchtest.resource.MAIN
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 
 class NewsFragment : Fragment() {
@@ -27,10 +32,21 @@ lateinit var binding: FragmentNewsBinding
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.btnBack.setOnClickListener {
-            MAIN.navController.navigate(R.id.action_secondFragment_to_firstFragment)
-        }
-    }
 
+        binding.rwNews.layoutManager = LinearLayoutManager(activity)
+
+        val apiInterface = ApiInterface.create().getNews()
+
+        apiInterface.enqueue( object : Callback<List<News>>{
+            override fun onResponse(call: Call<List<News>>?, response: Response<List<News>>?) {
+                val adapter = NewsAdapter(response?.body()!!)
+                binding.rwNews.adapter = adapter
+            }
+            override fun onFailure(call: Call<List<News>>?, t: Throwable?) {
+
+            }
+        })
+
+    }
 
 }
