5c02ff73a1547381f32e81df7a6e85f42f85caf0
Anton <imladris.doc@gmail.com>
2023-04-19 15:59:11 +0300

    закончил

--- app/src/main/java/com/example/betmatchtest/MainActivity.kt
+++ app/src/main/java/com/example/betmatchtest/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.betmatchtest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import com.example.betmatchtest.databinding.ActivityMainBinding
@@ -12,10 +13,15 @@ class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
     lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        //AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        //AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         navController = Navigation.findNavController(this, R.id.nav_host_fragment)
         MAIN = this
     }
+
+
 }
